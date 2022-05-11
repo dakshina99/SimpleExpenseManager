23564c25c5c572e90dd4622f0ada624dc0f350f8
Dakshina Ranmal <76484102+dakshina99@users.noreply.github.com>
2022-05-12 00:45:19 +0530

    testCases

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,6 +16,7 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -26,11 +27,16 @@ import androidx.test.core.app.ApplicationProvider;
 import org.junit.Test;
 import org.junit.BeforeClass;
 
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentDemoExpenseManager;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -54,11 +60,24 @@ public class ApplicationTest {
     }
 
     @Test
-    public void testAddExpence(){
+    public void testTransaction(){
         try{
+
+            testExpenceManager.updateAccountBalance("12345A",10, 4, 2022, ExpenseType.EXPENSE, "500");
+
             int sizeOfLogsArr = testExpenceManager.getTransactionLogs().size();
-            testExpenceManager.updateAccountBalance("12345A",10, 5, 2022, ExpenseType.EXPENSE, "500");
-            assertEquals(testExpenceManager.getTransactionLogs().size() , sizeOfLogsArr + 1);
+            Transaction last_transaction = testExpenceManager.getTransactionLogs().get(sizeOfLogsArr - 1);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd,MM,yyyy");
+            String strDate= formatter.format(last_transaction.getDate());
+
+            String[] testArr1 = {"12345A","500.0"};
+            String[] testArr2 = {last_transaction.getAccountNo(), String.valueOf(last_transaction.getAmount())};
+
+            assertEquals("10,05,2022", strDate);
+            assertEquals(ExpenseType.EXPENSE, last_transaction.getExpenseType());
+            assertArrayEquals(testArr1, testArr2);
+
         }catch (InvalidAccountException e){
 
         }
